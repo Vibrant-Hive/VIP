7059012cb5d1fa63ca2f192832f0be3b74c3a484
Abdul Arsin <abdularsin@gmail.com>
2022-05-24 02:17:39 +0530

    Firebase Hosting Enabled

--- src/app/service/register/register.service.ts
+++ src/app/service/register/register.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../model/User";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  private createAccountUrl: string = 'http://localhost:8080/createAccount';
+  private createAccountUrl: string = environment.baseUrl + '/createAccount';
 
   constructor(private _httpClient: HttpClient) {
   }
