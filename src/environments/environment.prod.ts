09633d9d50827151834531b48ecd06bec6d008bc
Abdul Arsin <abdularsin@gmail.com>
2023-01-25 12:53:20 +0530

    Added Azure Backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://vip-coders-service.herokuapp.com'
+  baseUrl: 'https://vibrant-hive-vibrant-hive-service-app.azuremicroservices.io'
 };
