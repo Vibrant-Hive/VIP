d234df51d1aaa506e228d2ae0cd366ae2df6a418
Abdul Arsin <abdularsin@gmail.com>
2023-03-02 19:56:13 +0530

    AZURE to AWS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://vibrant-hive-vibrant-hive-service-app.azuremicroservices.io'
+  baseUrl: 'http://vibranthiveservice-env.eba-m984kpsi.us-east-2.elasticbeanstalk.com'
 };
