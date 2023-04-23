f3554b0563c1d649bbde79e0770606991507d701
Abdul Arsin <abdularsin@gmail.com>
2023-04-23 18:25:34 +0530

    Python added

--- src/app/knowledge/knowledge.component.ts
+++ src/app/knowledge/knowledge.component.ts
@@ -52,20 +52,23 @@ export class KnowledgeComponent implements OnInit {
       case 'Database | SQL Basics':
         this.contentArr.push('If you have seen an Excel sheet,');
         this.contentArr.push('then you already know the basics of a database.');
-        this.contentArr.push('Install MySQL Community Server in your PC to start with.');
-        this.contentArr.push('<a href="https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-8.0.33.0.msi" target="_blank">Download Installer</a>');
+        this.contentArr.push('Refer: <a href="https://www.w3schools.com/sql/sql_intro.asp" target="_blank">W3Schools</a> <br>');
+        this.contentArr.push('<a></a>')
+        this.contentArr.push('Install MySQL Community Server in your PC to experiment');
+        this.contentArr.push('<a href="https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-8.0.33.0.msi" target="_blank">Download MySQL Installer</a>');
         break;
       case 'Frontend | HTML Basics':
         this.contentArr.push('Open <a href="https://www.amazon.in" target="_blank">Amazon</a> website with Chrome desktop,');
         this.contentArr.push('Right click on any button on the webpage,');
         this.contentArr.push('Select Inspect to see the HTML behind');
         break;
-      case 'Backend | Java Basics':
-        this.contentArr.push('Java is a language which has so many concepts.');
-        this.contentArr.push('As of now, try to understand just the basics mentioned.');
-        this.contentArr.push('<a href="https://www.w3schools.com/java/" target="_blank">W3Schools</a>');
-        this.contentArr.push('<a href="https://www.javatpoint.com/java-tutorial" target="_blank">JavaTpoint');
-        this.contentArr.push('<a href="https://www.tutorialspoint.com/java/index.htm" target="_blank">Tutorials Point</a>');
+      case 'Backend | Java/Python Basics':
+        this.contentArr.push('A programming language has so many concepts, don\'t worry.');
+        this.contentArr.push('As of now, try to understand just the basics like class, method, for, if, etc .');
+        this.contentArr.push('Refer: <a href="https://www.w3schools.com/" target="_blank">W3Schools</a>, <a href="https://www.javatpoint.com/" target="_blank">JavaTpoint</a>, <a href="https://www.tutorialspoint.com/" target="_blank">Tutorials Point</a>');
+        this.contentArr.push('<a></a>')
+        this.contentArr.push('Install IntelliJ Community Edition in your PC to experiment');
+        this.contentArr.push('<a href="https://www.jetbrains.com/idea/download/download-thanks.html?platform=windows&code=IIC" target="_blank">Download IntelliJ Installer</a>');
         break;
       case 'Frameworks':
         this.contentArr.push('Frameworks can be better explained only when you are ready with programming.');
@@ -76,6 +79,7 @@ export class KnowledgeComponent implements OnInit {
         this.contentArr.push('Create your own projects by with the acquired knowledge' );
         this.contentArr.push('<a href="https://start.spring.io/" target="_blank">Spring Initializer</a>' );
         this.contentArr.push('<a href="https://angular.io/start" target="_blank">Angular Documentation</a>');
+        this.contentArr.push('<a href="https://www.djangoproject.com/start/" target="_blank">Django Documentation</a>');
         break;
     }
   }
