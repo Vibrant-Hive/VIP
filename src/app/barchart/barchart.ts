c2e2287f46a3bb3e92bbc66d2a3c926565ffd742
Abdul Arsin <abdularsin@gmail.com>
2023-05-07 21:50:36 +0530

    certissss

--- src/app/barchart/barchart.ts
+++ src/app/barchart/barchart.ts
@@ -15,14 +15,14 @@ export class BarChartComponent {
   public systemData: ChartConfiguration<'bar'>['data'] = {
     labels: [ 'Self Learning', 'On Time', 'Consistency', 'Collaboration'],
     datasets: [
-      { data: [ 65, 59, 80, 81 ], label: 'System Calculated' },
+      { data: [ 80, 85, 95, 70 ], label: 'System Calculated' },
     ]
   };
 
   public mentorData: ChartConfiguration<'bar'>['data'] = {
     labels: [ 'Learning Spirit', 'Passion', 'Will Power', 'Synergy'],
     datasets: [
-      { data: [ 60, 69, 90, 71 ], label: 'Mentor Remarks' , backgroundColor: ["#4682B4", "#4682B4", "#4682B4", "#4682B4"]},
+      { data: [ 95, 70, 90, 80 ], label: 'Mentor Remarks' , backgroundColor: ["#4682B4", "#4682B4", "#4682B4", "#4682B4"]},
     ]
   };
 
--- src/app/learner/learner.component.ts
+++ src/app/learner/learner.component.ts
@@ -17,28 +17,53 @@ export class LearnerComponent implements OnInit {
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('userId')
     console.log(this.userId);
-    this.userStories=[
-      {
-        userStoryName: "VIP-Django: Setup a new Project with Mentors App",
-        userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16601961",
-        points: 5
-      },
-      {
-        userStoryName: "VIP-Django: Create User Model and migrate to mysql db",
-        userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16608647",
-        points: 3
-      },
-      {
-        userStoryName: "VIP-Django: Configure new mysql connection",
-        userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16609328",
-        points: 3
-      },
-      {
-        userStoryName: "VIP-Django: Create a new View named user_view.py",
-        userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16823882",
-        points: 5
-      }
-    ];
+    if(this.userId == 23) {
+      this.userStories = [
+        {
+          userStoryName: "VIP-Django: Setup a new Project with Mentors App",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16601961",
+          points: 5
+        },
+        {
+          userStoryName: "VIP-Django: Create User Model and migrate to mysql db",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16608647",
+          points: 3
+        },
+        {
+          userStoryName: "VIP-Django: Configure new mysql connection",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16609328",
+          points: 3
+        },
+        {
+          userStoryName: "VIP-Django: Create a new View named user_view.py",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive/projects/2/views/2?filterQuery=kavi&pane=issue&itemId=16823882",
+          points: 5
+        }
+      ];
+    } else if(this.userId == 0) {
+      this.userStories = [
+        {
+          userStoryName: "Project: User Story 1",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive",
+          points: 2
+        },
+        {
+          userStoryName: "Project: User Story 2",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive",
+          points: 3
+        },
+        {
+          userStoryName: "Project: User Story 3",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive",
+          points: 4
+        },
+        {
+          userStoryName: "Project: User Story 4",
+          userStoryLink: "https://github.com/orgs/Vibrant-Hive",
+          points: 5
+        }
+      ];
+    }
   }
 
   gridCols(): number {
@@ -50,11 +75,12 @@ export class LearnerComponent implements OnInit {
   }
 
   rowHeight() {
-    if (sessionStorage.getItem('device') === 'mobile') {
-      return "1:2";
-    } else {
-      return "2:3";
-    }
+    // if (sessionStorage.getItem('device') === 'mobile') {
+    //   return "1:2";
+    // } else {
+    //   return "2:3";
+    // }
+    return "600px";
   }
 
   githubBoard(story: any) {
